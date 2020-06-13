7bacae5931a3d33e419a41c0f00145820a251b87
jeonghoon <tkdgud9148@gmail.com>
2020-06-13 11:01:32 +0900

    [2020-06-13] CHAPTER 3 CLEAR

--- src/main/java/com/jojoldu/book/spring/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/spring/web/PostsApiController.java
@@ -18,13 +18,21 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
-    @PostMapping("/api/v1/posts/{id}")
+    @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
     }
+
+
 }
